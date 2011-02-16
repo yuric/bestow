5ba3c5c960119c5ffce3c503cbf74a18c35c5954
Lee-Ann Garrett <lnngarrett@Lee-Ann-Garretts-MacBook.local>
2011-02-15 23:58:18 -0800

    Sendgrid initializer started2

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require File.expand_path('../application', __FILE__)
 CARTOGRAPHER_GMAP_VERSION = 3
 # Initialize the rails application
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
+#config.action_mailer.delivery_method = :smtp
+#config.action_mailer.raise_delivery_errors = true
 Doutdes01::Application.initialize!
