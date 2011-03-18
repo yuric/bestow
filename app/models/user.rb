f95e72934dc93b852a5f6334361fedb1edbeab6c
Lee-Ann Garrett <lnngarrett@Lee-Ann-Garretts-MacBook.local>
2011-03-17 22:01:08 -0700

    confirmable false

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable and :activatable :confirmable\\ I did however generate the table for confirmable
   # it is just not added here for testing purposes
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable, #:confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :lockable
   validates_uniqueness_of :username#, :email
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation
 end
+#User.find(1).update_attributes(:admin => true)
+#User.find(1).confirm!
