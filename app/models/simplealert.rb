89ae2f701b6d6122df4e48865a2fba70ea12afb3
Lee-Ann Garrett <lnngarrett@Lee-Ann-Garretts-MacBook.local>
2011-03-18 01:54:22 -0700

    email confirmable working and simpleAlert validated

--- app/models/simplealert.rb
+++ app/models/simplealert.rb
@@ -1,4 +1,8 @@
 class Simplealert < ActiveRecord::Base
   
   validates :location_zip, :email, :presence => true
+  validates_format_of :email, :with =>/^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i,
+                              :message => "address is not valid"
+  validates_format_of :location_zip, :with =>/^(\d{5})(-\d{4})?$/,
+                              :message => "address is not valid"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable and :activatable :confirmable\\ I did however generate the table for confirmable
   # it is just not added here for testing purposes
-  devise :database_authenticatable, :registerable, :confirmable,# registarable allows new users to sign up
+  devise :database_authenticatable, :confirmable, :registerable,# registarable allows new users and I might HAVE to comment it out because I have a user_controller
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable
+         :lockable,:timeoutable
   validates_uniqueness_of :username#, :email
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation
