0a00f9172e28c73af76687b6e877b3f9e4627a58
Lee-Ann Garrett <lnngarrett@Lee-Ann-Garretts-MacBook.local>
2011-03-27 23:34:21 -0700

    empty commit:see previous

--- app/controllers/simplealerts_controller.rb
+++ app/controllers/simplealerts_controller.rb
@@ -52,7 +52,7 @@ class SimplealertsController < ApplicationController
   def create
     @simplealert = Simplealert.new(params[:simplealert])
     @simplealert.secrettoken = SecureRandom.hex(20)#using this to delete alerts
-    
+    #
     if user_signed_in?
      @simplealert.alert_owner = current_user.username     
     else
